6f156d8282a2b9babb5dc7dab19e25074ee56354
UpplySOPINTI <bart@upplyjobs.com>
2026-08-18 13:39:48 +0200

    Add API landing page

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'system-ui', padding: 40 }}>
+      <h1>Upply Integration API</h1>
+      <p>Status: online</p>
+    </main>
+  );
+}
